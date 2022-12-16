1a41a1b23c288f35d49a18c9322e2ceca0851a36
Felice <96291397+Felicewulfse@users.noreply.github.com>
2022-12-16 14:09:53 +0100

    Add files via upload
    Signed-off-by: Felice <96291397+Felicewulfse@users.noreply.github.com>

--- Analysis_script.py
+++ Analysis_script.py
@@ -23,7 +23,6 @@ import seaborn as sns
 
 
 
-
 #%%  
 
 # STEP 2: CREATE VARIABLES
@@ -41,6 +40,7 @@ light_cycle_dataframe = pd.DataFrame({'light_cycle':[1,0,1,0,1,0,1,0,1,0],
                                               'danlab', 'hoferlab', 'mainenlab', 'mrsicflogellab', 'wittenlab','zadorlab']}) # Merge the light cycle values to the lab_names
 df_subjects = pd.merge(df_subjects, light_cycle_dataframe, on = 'lab_name') # Save the light cycle values to the subjects data frame
 df_subjects.columns # Inspect the columns again (check whether the merging worked and whether each subject has a light cycle value)
+subjects_light_cycle = df_subjects[['subject_uuid','light_cycle']]
 
 
 # Investigate the subject id's 
@@ -54,6 +54,7 @@ total_number_sessions = df_sessions.groupby(['subject_uuid'])['session_start_tim
 
 # Create a total number of training days variable
 total_number_training_days = df_sessions.groupby(['subject_uuid'])['session_dates'].count().reset_index(name = 'total_number_training_days') # Count the total number of training days for each subject
+
 
 # Create a session number per subject variable 
 List_subject_uuids = df_sessions['subject_uuid'].unique() # Create a list with all unique subject_uuid's in the sessions data frame
@@ -86,20 +87,70 @@ consistency_df = pd.merge(max_per_subject, sum_per_subject, on = 'subject_uuid')
 consistency_df['time_consistency'] = consistency_df['max']/consistency_df['sum'] # Use the maximum frequency and the total frequency of the training time to calculate the consistency in training time for each subject
 
 
-# Create learning speed variable
+
+# Create different learning_speed variables
+
+# First version (speed by performance high-low/total sessions)
 performance_start = df_sessions[filters].groupby(['subject_uuid'])['performance_easy'].min().reset_index(name = 'performance_start') # Save the minimum performance for each subject
 performance_end = df_sessions[filters].groupby(['subject_uuid'])['performance_easy'].max().reset_index(name = 'performance_end') # Save the maximum performance for each subject
+
 learning_speed_df = pd.merge(performance_end, performance_start, on = "subject_uuid") # Merge the data frames with start and end performance for each subject
 learning_speed_df = pd.merge(learning_speed_df, total_number_sessions, on = "subject_uuid") # Also save the total number of sessions to this data frame
 learning_speed_df = pd.merge(learning_speed_df, total_number_training_days, on = "subject_uuid") # Also save the total number of training days to this data frame
-learning_speed_df['learning_speed'] = (learning_speed_df['performance_end'] - learning_speed_df['performance_start'])/learning_speed_df['total_number_sessions'] # Create the training_speed variable by dividing the performance difference by the total number of sessions for each individual
+learning_speed_df['learning_speed_1'] = (learning_speed_df['performance_end'] - learning_speed_df['performance_start'])/learning_speed_df['total_number_sessions'] # Create the training_speed variable by dividing the performance difference by the total number of sessions for each individual
 
+
+# Second version (speed by performance last_day-first_day/total sessions)
+session_start = df_sessions[filters].groupby(['subject_uuid'])['session_number'].min().reset_index(name = 'session_number') # Save the row of the first session per subject
+session_end = df_sessions.groupby(['subject_uuid'])['session_number'].max().reset_index(name = 'session_number') # Save the row of the last session per subject
+
+subject_session_performance = df_sessions[['subject_uuid','session_number','performance_easy' ]] # Create new dataframe with only the subject id, the session number and the performance
+performance_session_start = pd.merge(session_start, subject_session_performance, how = 'inner', on = ['subject_uuid', 'session_number']) # Merge dataframes with only the first session to save the performance at the first session
+performance_session_end = pd.merge(session_end, subject_session_performance, how = 'inner', on = ['subject_uuid', 'session_number']) # Merge dataframes with only the last session to save the performance at the last session
+performance_session_start = performance_session_start[['subject_uuid', 'performance_easy']] # Remove the session number
+performance_session_end = performance_session_end [['subject_uuid', 'performance_easy']] # Remove the session number
+performance_session_start = performance_session_start.rename(columns = {'performance_easy': 'performance_session_start'}) # Change the colname
+performance_session_end = performance_session_end.rename(columns = {'performance_easy': 'performance_session_end'}) # Change the colname
+                                          
+performance_sessions = pd.merge(performance_session_start, performance_session_end, on = 'subject_uuid') # Merge the performance at the start and the performance at the end
+learning_speed_df = pd.merge(learning_speed_df, performance_sessions, on = "subject_uuid") # Merge the performance start and end to the learning speed data frame
+learning_speed_df['learning_speed_2'] = (learning_speed_df['performance_session_end'] - learning_speed_df['performance_session_start'])/learning_speed_df['total_number_sessions'] # Create the training_speed variable by dividing the performance difference by the total number of sessions for each individual
+
+
+# Third version (sessions to trained)
+trained_animals = df_sessions.loc[(df_sessions['training_status'] == "trained_1a") | (df_sessions['training_status'] == "trained_1b")] # Save the rows where the training_status is trained_1a or trained_1b
+trained_animals_only = trained_animals.groupby('subject_uuid')['session_number'].min().reset_index(name = 'session_to_trained') # Save the minimal session_number per subject 
+learning_speed_df = pd.merge(learning_speed_df, trained_animals_only, on ='subject_uuid') # Merge the session_to_trained variable with the learning_speed_df
+learning_speed_df = learning_speed_df.drop_duplicates() # Remove duplicate observations
+
+
+# Fourth version (sessions to 80% of performance easy)
+performance_end[['eighty_performance_end']] = performance_end[['performance_end']]*0.8 # Save 80% of the performance
+performence_eight = performance_end[['subject_uuid', 'eighty_performance_end']] # Save the value in a new dataframe
+df_sessions = pd.merge(df_sessions, performance_end, on = 'subject_uuid') # Merge this value to the sessions dataframe
+index_eighty = np.where(df_sessions['performance_easy'] >= df_sessions['eighty_performance_end'] ) # Find where the performance is similar or greater than 80% for each individual
+df_sessions_eighty = df_sessions.iloc[index_eighty[0]] # Save these rows
+df_sessions_eighty_over = df_sessions_eighty.groupby('subject_uuid')['session_number'].min().reset_index(name = 'sessions_to_eighty') # Get the session number for the observations where the performance is greater than 80%
+df_sessions_eighty_over = df_sessions_eighty_over[['subject_uuid', 'sessions_to_eighty']] # Only save the number to the data frame
+learning_speed_df = pd.merge(learning_speed_df, df_sessions_eighty_over, on = "subject_uuid") # Merge this to the dataframe
+
+
+
+#%%  
+
+# Analyse the different learning speed variables
+sns.pairplot(learning_speed_df[['learning_speed_1', 'learning_speed_2', 'session_to_trained', 'sessions_to_eighty']])
+plt.show()
+
+
+#%%  
 # Create analysis dataframe with all variables created above
 training_times_df = pd.merge(average_training_time, most_frequent_time, on = 'subject_uuid') # Merge the average training time and the most frequent training time ..
 training_times_df = pd.merge(training_times_df, consistency_df, on = 'subject_uuid') # .. with the consistency per subject data frame
 
 analysis_dataframe = pd.merge(learning_speed_df, training_times_df, on = 'subject_uuid') # Merge all variables created together into one data frame
-
+analysis_dataframe = pd.merge(analysis_dataframe, subjects_light_cycle, on = 'subject_uuid')
+analysis_dataframe.columns
 
 
 #%%  
@@ -107,6 +158,14 @@ analysis_dataframe = pd.merge(learning_speed_df, training_times_df, on = 'subjec
 # STEP 3: ANALYSIS
 from statsmodels.formula.api import ols
 
-ols_model = ols(formula = 'learning_speed ~ time_consistency', data = analysis_dataframe)
+ols_model = ols(formula = 'learning_speed ~ time_consistency + light_cycle', data = analysis_dataframe)
 ols_result = ols_model.fit()
 ols_result.summary()
+
+
+ols_model_all = ols(formula = 'learning_speed ~ performance_end + performance_start + total_number_sessions + total_number_training_days + average_training_time + most_frequent_time + max + sum + time_consistency + light_cycle',  data = analysis_dataframe)
+ols_result_all = ols_model.fit()
+ols_result_all.summary()
+
+
+
